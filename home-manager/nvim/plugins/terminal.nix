8776cbf36782c4b1ed29d91b2dc30477da2c4652
henrispriet <henri.spriet@gmail.com>
2024-06-21 13:13:42 +0200

    nixvim make :Do run in interactive shell
    this way i can use my aliases and stuff
    makes it align more to what i want
    which is "run this one command, but i dont wanna open a whole terminal,
    because its just the one command"
    instead of :! which is more for code to call i feel like

--- home-manager/nvim/plugins/terminal.nix
+++ home-manager/nvim/plugins/terminal.nix
@@ -16,13 +16,14 @@
     # https://github.com/akinsho/toggleterm.nvim/?tab=readme-ov-file#custom-terminals
     # + maybe a command to send lines to bg term (BgSendLine)? https://github.com/akinsho/toggleterm.nvim/?tab=readme-ov-file#sending-lines-to-the-terminal
 
-    # TODO: make :! map to Do
-    # possible via autocmds?
+    # run one shell command (in an interactive bash shell)
+    # and show the ouput in a floating toggleterm window
     userCommands."Do" = {
       desc = "Run a shell command";
       nargs = "+";
       complete = "shellcmd";
       command.__raw = let
+        # more colorful exit status message than the one provided by close_on_exit = false
         postCommand = pkgs.writeShellScript "check-return" ''
           EXIT=$?
           if [[ $EXIT == 0 ]]; then
@@ -39,7 +40,7 @@
         -- https://github.com/akinsho/toggleterm.nvim/?tab=readme-ov-file#custom-terminals
         function(command)
           require('toggleterm.terminal').Terminal:new({
-            cmd = command.args.."; . ${postCommand}",
+            cmd = "bash -ic '"..command.args.."; . ${postCommand}'",
             display_name = command.fargs[1],
             direction = "float",
             close_on_exit = true,
