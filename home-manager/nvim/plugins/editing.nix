763f5d176e64ff6510f9ea00a2b559f8b6071181
henrispriet <henri.spriet@gmail.com>
2024-06-22 11:52:30 +0200

    mini.pairs -> nvim-autopairs
    - has better defaults
     - has more deeply customizable rules (e.g. using treesitter) which will
    allow me to completely finetune these in the future

--- home-manager/nvim/plugins/editing.nix
+++ home-manager/nvim/plugins/editing.nix
@@ -13,41 +13,45 @@
 
       indent-blankline.enable = true;
 
+      # TODO: finetune pair rules
+      # add `<>` and `||` with _correct_ rules (for use in Rust)
+      nvim-autopairs.enable = true;
+
       mini = {
         enable = true;
 
         modules = {
-          pairs = {};
           comment = {};
-        };
-      };
-      mini.modules.surround = {
-        respect_selection_type = true;
-        search_method = "cover_or_next";
-        silent = true;
 
-        # swaps default behaviour of left bracket and right bracket for all bracket types
-        # ok i admit, i had a little fun here...
-        custom_surroundings = with lib;
-          pipe ["()" "[]" "{}" "<>"]
-          [
-            (map stringToCharacters)
-            (map (pair: {
-              ${head pair} = pair;
-              ${last pair} = pair;
-            }))
-            (foldl mergeAttrs {})
-            (mapAttrs (
-              name: pair: let
-                space = optionalString (name == (last pair)) " ";
-              in {
-                output = {
-                  left = (head pair) + space;
-                  right = space + (last pair);
-                };
-              }
-            ))
-          ];
+          surround = {
+            respect_selection_type = true;
+            search_method = "cover_or_next";
+            silent = true;
+
+            # swaps default behaviour of left bracket and right bracket for all bracket types
+            # ok i admit, i had a little fun here...
+            custom_surroundings = with lib;
+              pipe ["()" "[]" "{}" "<>"]
+              [
+                (map stringToCharacters)
+                (map (pair: {
+                  ${head pair} = pair;
+                  ${last pair} = pair;
+                }))
+                (foldl mergeAttrs {})
+                (mapAttrs (
+                  name: pair: let
+                    space = optionalString (name == (last pair)) " ";
+                  in {
+                    output = {
+                      left = (head pair) + space;
+                      right = space + (last pair);
+                    };
+                  }
+                ))
+              ];
+          };
+        };
       };
     };
   };
